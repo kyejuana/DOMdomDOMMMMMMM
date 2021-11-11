f8814d6e0503095de83e34e5b33b9ce8dc8576ce
KyeJuana Avery <kyejuanaavery@gmail.com>
2021-11-11 17:02:36 -0600

    commit message

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.createElement('button');
-    let btnText = document.createTextNode('add square');
-    button.className = 'button';
-    button.appendChild(btnText);
-    document.body.appendChild(button);
-    let id = 0;
-        
-    button.addEventListener('click', buttonClick);
-    function buttonClick() {
-        id++;     
-        let square = document.createElement('div');
-        square.className = 'square';
-        square.style.width = 30px;
-        square.style.height = 30px;
-        square.style.color = purple;
-        document.body.appendChild(square);
-    }
-   
+const addSquareButton = document.createElement("button");
+addSquareButton.textContent = "Add Square";
+document.body.appendChild(addSquareButton);
+
+const squareContainer = document.createElement("div");
+squareContainer.id = "square-container";
+document.body.appendChild(squareContainer);
+
+let numOfSquares = 0;
+
+addSquareButton.addEventListener("click", function () {
+    const newDiv = document.createElement("div");
+    newDiv.className = "square";
+    // newDiv.style.backgroundColor = "black";
+    // newDiv.style.height = "100px";
+    // newDiv.style.width = "100px";
+
+    numOfSquares++
+    newDiv.id = numOfSquares;
+
+    newDiv.addEventListener("mouseover", function () {
+        newDiv.textContent = newDiv.id;
+    });
+
+    newDiv.addEventListener("mouseout", function () {
+        newDiv.textContent = "";
+    });
+
+    newDiv.addEventListener("click", function () {
+        newDiv.style.backgroundColor = getRandomColor();
+    });
+
+    newDiv.addEventListener("dblclick", function () {
+        if (newDiv.id % 2 == 0) {
+            if (newDiv.nextSibling) {
+                newDiv.nextSibling.remove();
+            } else {
+                alert("No Square!");
+            }
+        } else {
+            if (newDiv.previousSibling) {
+                newDiv.previousSibling.remove();
+            } else {
+                alert("No Square!");
+            }
+        }
+    });
+    
+    squareContainer.appendChild(newDiv);
 });
 
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
